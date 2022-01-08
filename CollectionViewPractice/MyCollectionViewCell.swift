a8790baea4b7cb451a202261d2d903a973975c77
Jager-yoo <y73447jh@gmail.com>
2022-01-08 22:12:02 +0900

    CollectionView 파라미터 조정
    - verticalScrollIndicator 안 보이도록 false 값 부여
    - numberOfSections 1개 -> 2개로 수정
    - 클로저를 통해 UILabel 속성을 바로 지정해서 리턴하는 방법 메모

--- CollectionViewPractice/MyCollectionViewCell.swift
+++ CollectionViewPractice/MyCollectionViewCell.swift
@@ -10,4 +10,11 @@ import UIKit
 class MyCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var emojiLabel: UILabel!
+    
+    let codeLabel: UILabel = { // UILabel 은 클래스.
+        let label = UILabel()
+        label.text = "요호호"
+        return label
+    }() // 열고 닫는 소괄호()가 없으면 그냥 클로저임. 매개변수랑 리턴 타입을 갖는 '타입'이라는 말임.
+    // 소괄호를 열고 닫았다는 건, 클로저를 '실행' 시켰다는 것임. 그 리턴값을 다시 uiLabel 에 집어넣게 된다는 뜻
 }
--- CollectionViewPractice/ViewController.swift
+++ CollectionViewPractice/ViewController.swift
@@ -20,20 +20,20 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         // UICollectionViewDelegateFlowLayout 프로토콜은 UICollectionViewDelegate 프로토콜을 상속받음.
         emojiCollectionView.delegate = self
         emojiCollectionView.dataSource = self
+        emojiCollectionView.showsVerticalScrollIndicator = false
         emojiCollectionView.isPagingEnabled = true
-        
-        // 스토리보드에서 컬렉션 뷰 레이아웃 쓸 거면, 이런 게 필요가 없다고!! (추정)
         emojiCollectionView.collectionViewLayout = flowLayout
+        
         flowLayout.scrollDirection = .horizontal
         flowLayout.minimumLineSpacing = .zero
-        
         flowLayout.itemSize = CGSize(width: view.bounds.size.width, height: emojiCollectionView.bounds.size.height)
     }
     
     // 이 메서드를 통해 section 개수를 바꿔줄 수 있다. 안 바꾸면 1 임
-//    func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        return 1
-//    }
+    // 만약 section 개수를 2개로 올리면, section 하나당 8개의 이모지가 있었는데 x 2 해서 -> 16개의 이모지가 나타남
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
     
     // 데이터가 몇 개인지? -- section 은 디폴트로 1개로 잡혀있음.
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
