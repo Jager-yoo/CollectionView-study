97538c5c8b68d2fe7f5a3053b4c3c57c891cc457
Jager-yoo <y73447jh@gmail.com>
2022-01-08 20:38:38 +0900

    feat: 2 X 8 grid 구조의 CollectionView 구현

---
+++ CollectionViewPractice/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionViewPractice
+//
+//  Created by 유재호 on 2022/01/08.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+}
--- CollectionViewPractice/ViewController.swift
+++ CollectionViewPractice/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var emojiCollectionView: UICollectionView!
+    let emojies = ["🔥", "🥰", "🥲", "👍", "👨‍🔬", "🤪", "🐸", "⚾️"]
+    // 스토리보드의 [Collection View Flow Layout] 를 사용할 거라면, 아래처럼 굳이 선언할 필요가 없다고 함.
+    let flowLayout = UICollectionViewFlowLayout()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // UICollectionViewDelegateFlowLayout 프로토콜은 UICollectionViewDelegate 프로토콜을 상속받음.
+        emojiCollectionView.delegate = self
+        emojiCollectionView.dataSource = self
+        // 스토리보드에서 컬렉션 뷰 레이아웃 쓸 거면, 이런 게 필요가 없다고!! (추정)
+        emojiCollectionView.collectionViewLayout = flowLayout
+        
+        let size = (view.bounds.size.width / 4 - 20)
+        flowLayout.scrollDirection = .vertical
+        flowLayout.itemSize = CGSize(width: size, height: size)
+    }
+    
+    // 이 메서드를 통해 section 개수를 바꿔줄 수 있다. 안 바꾸면 1 임
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return 1
+//    }
+    
+    // 데이터가 몇 개인지? -- section 은 디폴트로 1개로 잡혀있음.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emojies.count
+    }
+    
+    // 데이터가 무엇인지?
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = emojiCollectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionViewCell", for: indexPath) as! MyCollectionViewCell
+        
+        cell.emojiLabel.text = emojies[indexPath.item]
+        cell.layer.cornerRadius = 8
+        cell.backgroundColor = .systemYellow
+        
+        return cell
     }
 
-
+    //
 }
 
